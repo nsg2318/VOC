eacb96beabb5fe5f3367d5272bd9c9b282da6c57
sunba <nsg2318@naver.com>
2022-04-30 16:12:43 +0900

    ADD : SampleData 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,12 @@
-insert into supplier values('1','SEON','MN','010');
-insert into carrier values('1','BM','BAEDAL');
+insert into supplier values('1','VENDOR001','MANAGER1','010-0101-0101');
+insert into supplier values('2','VENDOR002','MANAGER2','010-0201-0102');
+insert into supplier values('3','VENDOR003','MANAGER3','010-0301-0103');
+insert into supplier values('4','VENDOR004','MANAGER4','010-0401-0104');
+insert into supplier values('5','VENDOR005','MANAGER5','010-0501-0105');
+
+insert into carrier values('1','DRIVER001','BAEDAL');
+insert into carrier values('2','DRIVER002','BAEDAL');
+insert into carrier values('3','DRIVER003','BULLET');
+insert into carrier values('4','DRIVER004','BULLET');
+insert into carrier values('5','DRIVER005','ROCKET');
+insert into carrier values('6','DRIVER006','ROCKET');
